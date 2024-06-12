0123af34a99896f18080a2c72c229a0e68079e9a
Laptop <adamjednacz4@gmail.com>
2024-06-12 13:15:35 +0200

    service

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -2,6 +2,8 @@ import {Component, Input} from "@angular/core";
 import {TaskComponent} from "./task/task.component";
 import {NewTaskComponent} from "./new-task/new-task.component";
 import {NewTaskData} from "./task/task.model";
+import {TasksService} from "./tasks.service";
+
 
 @Component({
   selector:'app-tasks',
@@ -15,53 +17,24 @@ export class TasksComponent {
   @Input({required:true})name! :string
   @Input({required:true})userId!:string
   isAddingTask = false;
-  tasks = [
-    {
-      id:'t1',
-      userId:'u1',
-      title:'Master Angular',
-      summary:'How to basic',
-      dueDate:'2025-12-31'
-    },
-    {
-      id:'t2',
-      userId:'u3',
-      title:'Build first prototype',
-      summary:'Build a first prototype od the online shop website',
-      dueDate:'2025-05-31'
-    },
-    {
-      id:'t3',
-      userId:'u3',
-      title:'Master Angular',
-      summary:'Prepare and describe an issue template which will help with project management',
-      dueDate:'2025-06-15'
-    }
-  ]
+
+
+  constructor(private tasksService:TasksService) {
+
+  }
   get selectedUserTasks(){
-    return this.tasks.filter((task) =>task.userId === this.userId )
+    return this.tasksService.getUserTasks(this.userId)
   }
 
   onCompleteTask(id:string){
-      this.tasks=this.tasks.filter((task) => task.id !== id)
+    return this.tasksService.removeTask(this.userId)
   }
-
 
   onStartAddTask(){
     this.isAddingTask = true;
 
   }
-  onCancelAddTask(){
+  onCloseAddTask(){
     this.isAddingTask = false
-  }
-  onAddTask(taskData:NewTaskData){
-    this.tasks.unshift({
-      id:new Date().getTime().toString(),
-      userId: this.userId,
-      title: taskData.title,
-      summary:taskData.summary,
-      dueDate:taskData.date
-    })
-    this.isAddingTask = false;
   }
 }
