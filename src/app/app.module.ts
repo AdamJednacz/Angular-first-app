067c85b31c2d91feaf2eb9afb8264cd5ce8d9795
AdamJednacz <adamjednacz4@gmail.com>
2024-06-14 23:52:21 +0200

    browsermodule

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import {NgModule} from "@angular/core";
+
+import {AppComponent} from "./app.component";
+import {BrowserModule} from "@angular/platform-browser";
+import {UserComponent} from "./user/user.component";
+import {TasksComponent} from "./tasks/tasks.component";
+
+import {HeaderComponent} from "./header/header.component";
+@NgModule({
+  declarations:[AppComponent],
+  imports:[BrowserModule,HeaderComponent,UserComponent,TasksComponent],
+  bootstrap:[AppComponent]
+})
+export class AppModule {
+
+}
