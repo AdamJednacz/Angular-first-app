6a7d8c31a4ba6e40e833524664378094399b2d16
AdamJednacz <adamjednacz4@gmail.com>
2024-06-15 18:49:26 +0200

    moduły z podmodułami

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import {NgModule} from "@angular/core";
-
-import {AppComponent} from "./app.component";
+import {FormsModule} from "@angular/forms";
 import {BrowserModule} from "@angular/platform-browser";
+import {AppComponent} from "./app.component";
+
 import {UserComponent} from "./user/user.component";
 import {TasksComponent} from "./tasks/tasks.component";
-
 import {HeaderComponent} from "./header/header.component";
+import {TaskComponent} from "./tasks/task/task.component";
+import {NewTaskComponent} from "./tasks/new-task/new-task.component";
+import {SharedModule} from "./shared/shared.module";
+
+
 @NgModule({
-  declarations:[AppComponent],
-  imports:[BrowserModule,HeaderComponent,UserComponent,TasksComponent],
+  declarations:[
+    AppComponent,
+    HeaderComponent,
+    UserComponent,
+    TasksComponent,
+    TaskComponent,
+    NewTaskComponent],
+  imports:[BrowserModule,FormsModule,SharedModule],
   bootstrap:[AppComponent]
 })
 export class AppModule {
